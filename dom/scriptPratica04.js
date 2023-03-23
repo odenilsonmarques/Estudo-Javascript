9e4e852a1458eda620a2f6644eff11046db04c82
Odenilson Marques <odenilson.marques@gmz.solutions>
2023-03-23 15:07:20 -0300

    estudo querySelector

---
+++ dom/scriptPratica04.js
@@ -0,0 +1,5 @@
+// console.log(document.getElementsByClassName('destaque'))
+
+
+//usado dessa forma quando se tem mias de ume elemento
+document.getElementsByClassName('destaque')[0].innerHTML = '<h1>Boa tarde</h1>'
---
+++ dom/scriptPratica05.js
@@ -0,0 +1,1 @@
+console.log(document.querySelector('div').innerHTML = '<strong>EU spu uma div</strong>')
